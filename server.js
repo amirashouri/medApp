4364bf5d498a0c3732d2e9b97870d5493fcb46f4
amirreza <amirashouri72@gmail.com>
2020-06-13 17:09:00 +0430

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mustacheExpress = require("mustache-express");
+
+const app = express();
+
+const mustache = mustacheExpress();
+mustache.cache = null;
+app.engine("mustache", mustache);
+app.set("view engine", "mustache");
+
+app.use(express.static("public"));
+
+app.get("/meds", (req, res) => {
+  res.render("meds");
+});
+
+app.listen(5001, () => {
+  console.log("listening to port 5001");
+});
